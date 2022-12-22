af78454f37a334072d8ecf4939baead4a822579e
Rafael Sabater Carbonell <rsc65@alu.ua.es>
2022-12-22 16:19:27 +0100

    Escuela añadida al código fuente.

--- dca.py
+++ dca.py
@@ -4,6 +4,7 @@ def main():
     print("Desarrollo Colaborativo de Aplicaciones\n", end="")
     print("Curso 2022/2023\n", end="")
     print("Universidad de Alicante\n", end="")
+    print("Escuela Politécnica Superior\n")
 
 if __name__ == '__main__':
     main()
