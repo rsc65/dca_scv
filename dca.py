e2054118924e7046c9dcb2912ad5fc2de501f502
Rafael Sabater Carbonell <rsc65@alu.ua.es>
2022-12-22 15:34:53 +0100

    Universidad añadida al código.

--- dca.py
+++ dca.py
@@ -3,6 +3,7 @@
 def main():
     print("Desarrollo Colaborativo de Aplicaciones\n", end="")
     print("Curso 2022/2023\n", end="")
+    print("Universidad de Alicante\n", end="")
 
 if __name__ == '__main__':
     main()
