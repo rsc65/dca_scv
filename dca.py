9fccf151b8a84be76da9628d8369fdb2ec68bf2b
Rafael Sabater Carbonell <rsc65@alu.ua.es>
2022-12-22 16:20:19 +0100

    Grado añadido al código fuente.

--- dca.py
+++ dca.py
@@ -3,6 +3,7 @@
 def main():
     print("Desarrollo Colaborativo de Aplicaciones\n", end="")
     print("Curso 2022/2023\n", end="")
+    print("Ingeniería Informática\n", end="")
     print("Universidad de Alicante\n", end="")
     print("Escuela Politécnica Superior\n")
 
