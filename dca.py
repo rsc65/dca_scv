4c28af60d042ad7217a885512e34ba16d175f3ab
Rafael Sabater Carbonell <rsc65@alu.ua.es>
2022-12-22 16:36:22 +0100

    Especialidad añadida al código.

--- dca.py
+++ dca.py
@@ -4,6 +4,7 @@ def main():
     print("Desarrollo Colaborativo de Aplicaciones\n", end="")
     print("Curso 2022/2023\n", end="")
     print("Ingeniería Informática\n", end="")
+    print("Ingeniería del Software\n", end="")
     print("Universidad de Alicante\n", end="")
     print("Escuela Politécnica Superior\n")
 
